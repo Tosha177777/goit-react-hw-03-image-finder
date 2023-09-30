488b198a76ed26e390c4c7aabdddbf2faa6693fb
Tosha177777 <slavkoanton4@gmail.com>
2023-09-30 19:18:25 +0300

    css

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,11 +4,9 @@ import css from '../../Styles.module.css';
 const Button = ({ handleClick, showImages }) => {
   return (
     showImages && (
-      <div className={css.ButtonContainer}>
-        <button type="button" className={css.Button} onClick={handleClick}>
-          Load more
-        </button>
-      </div>
+      <button type="button" className={css.Button} onClick={handleClick}>
+        Load more
+      </button>
     )
   );
 };
